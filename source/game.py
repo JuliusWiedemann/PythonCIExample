74dd7230daeeeb5ddf6381d5d826d5a8837a8924
Julius Wiedemann <juliuswiedemann01@gmail.com>
2024-06-10 17:27:05 +0200

    Add type hints to souce code and add mypy.ini file

--- source/game.py
+++ source/game.py
@@ -6,35 +6,35 @@ import time
 # pylint: disable=E0401 # False positive
 from pokemon import Pokemon
 
-def welcome():
+def welcome() -> None:
     """
     Prints the welcome message
     """
     print("Welcome to the pokemon game!")
 
-def createNewPokemon():
+def createNewPokemon() -> Pokemon | bool:
     """
     Lets the user create a new pokemon via text input
     Returns the new pokemon
     """
-    name = input("Please enter the name of your pokemon: ")
+    name: str = input("Please enter the name of your pokemon: ")
     if name is None or name == "":
         print("Invalid name")
         return False
 
     try:
-        number = int(input("Please enter the number of your pokemon: "))
+        number: int = int(input("Please enter the number of your pokemon: "))
     except ValueError:
         print("Invalid number")
         return False
 
-    pokeType = input("Please enter the type of your pokemon: ")
+    pokeType: str = input("Please enter the type of your pokemon: ")
 
-    newPokemon = Pokemon(name, number, pokeType)
-    print(f"Your created the following pokemon: {newPokemon}")
+    newPokemon: Pokemon = Pokemon(name, number, pokeType)
+    print(f"You created the following pokemon: {newPokemon}")
     return newPokemon
 
-def attackPokemon(pokemonStorage):
+def attackPokemon(pokemonStorage: dict) -> bool:
     """
     Allows the user to attack a pokemon
     """
@@ -42,55 +42,55 @@ def attackPokemon(pokemonStorage):
         print("You need at least 2 pokemon to fight.")
         return False
 
-    name1 = input("Enter the name of your pokemon: ")
-    pokemon1 = getPokemon(pokemonStorage, name1)
+    name1: str = input("Enter the name of your pokemon: ")
+    pokemon1: Pokemon = getPokemon(pokemonStorage, name1)
     if not pokemon1:
         return False
 
     name2 = input("Enter the name of the pokemon to attack: ")
-    pokemon2 = getPokemon(pokemonStorage, name2)
+    pokemon2: Pokemon = getPokemon(pokemonStorage, name2)
     if not pokemon2:
         return False
 
     pokemon1.attack(pokemon2)
     return True
 
-def viewPokemonStats(pokemonStorage):
+def viewPokemonStats(pokemonStorage: dict) -> None:
     """
     Allows the user to view stats of one pokemon he created
     """
     name = input("Enter the name of your pokemon: ")
 
-    pokemon = getPokemon(pokemonStorage, name)
+    pokemon: Pokemon = getPokemon(pokemonStorage, name)
     if pokemon:
         print(pokemon)
 
-def getPokemon(pokemonStorage, name):
+def getPokemon(pokemonStorage: dict, name: str) -> Pokemon | bool:
     """
     Searches a pokemon by name from the pokemon storage and returns it
     Returns false if pokemon does not exist
     """
-    pokemon = pokemonStorage.get(name)
+    pokemon: Pokemon = pokemonStorage.get(name)
     if pokemon is None:
         print(f"Sorry. The pokemon {name} does not exist.")
         return False
     return pokemon
 
-def main():
+def main() -> None:
     """
     Creates while loop which lets the user create and fight pokemon via text input
     """
-    game = True
-    pokemonStorage = {}
+    game: bool = True
+    pokemonStorage: dict = {}
 
     welcome()
     while game:
         print("\n\n--------------------------------\n\n")
 
-        choice = input("What to you want to do?\n1: Create new pokemon\n2: Attack a pokemon\n3: View Stats\nQ: Quit Game\n")
+        choice: str = input("What to you want to do?\n1: Create new pokemon\n2: Attack a pokemon\n3: View Stats\nQ: Quit Game\n")
 
         if choice == "1":
-            newPokemon = createNewPokemon()
+            newPokemon: Pokemon = createNewPokemon()
             if newPokemon:
                 pokemonStorage[newPokemon.getName()] = newPokemon
         elif choice == "2":
--- source/pokemon.py
+++ source/pokemon.py
@@ -1,7 +1,6 @@
 """
 Module to control pokemon and different poke types.
 """
-
 import random
 
 class Pokemon:
@@ -9,30 +8,32 @@ class Pokemon:
     Class with all relevant data for a Pokemon.
     Functions to attack another Pokemon.
     """
-    def __init__(self, name, number, pokeType):
+    def __init__(self, name: str, number: int, pokeType: str):
         self._name = name
         self._number = number
         self._pokeType = pokeType
-        self._health = 100
-        self._maxHealth = 100
-        self._level = 1
-        self._levelProgress = 0
-        self._isAlive = True
-        self._strength = random.randint(1, 30)
+        self._health: int = 100
+        self._maxHealth: int = 100
+        self._level: int = 1
+        self._levelProgress: int = 0
+        self._isAlive: bool = True
+        self._strength: int = random.randint(1, 30)
 
-    def __eq__(self, other):
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, Pokemon):
+            return NotImplemented
         return self._name == other.getName()
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"Name: {self._name}\nType: {self._pokeType}\nLevel: {self._level}\nHealth: {self._health}\nStrength: {self._strength}"
 
-    def _levelUp(self):
+    def _levelUp(self) -> None:
         self._level += 1
         print(f"Pokemon {self._name} is now level {self._level}!")
         self._maxHealth += 10
         self._health += 10
 
-    def attack(self, opponent):
+    def attack(self, opponent: "Pokemon") -> None:
         """
         Attacks another pokemon by calculating the attack factor.
         After the fight, the attacking pokemon receives XP.
@@ -43,18 +44,18 @@ class Pokemon:
             print("Opponent is dead already!")
         else:
             print(f"{self._name} attacks {opponent.getName()}!")
-            attackFactor = self.getAttackFactor(self._pokeType, opponent.getPokeType())
-            attackDamage = self._strength * attackFactor
-            if attackFactor == 1:
+            attackFactor: int = self.getAttackFactor(self._pokeType, opponent.getPokeType())
+            attackDamage: int = self._strength * attackFactor
+            if attackFactor == 2:
                 print("Effective")
-            elif attackFactor == 0.5:
+            elif attackFactor == 1:
                 print("Not very effective")
-            elif attackFactor == 2:
+            elif attackFactor == 3:
                 print("Very effective")
             opponent.receiveDamage(attackDamage)
             self.earnXp(attackDamage)
 
-    def receiveDamage(self, damage):
+    def receiveDamage(self, damage: int) -> None:
         """
         Reduces the health of a pokemon by amount of damage.
         and checks if the pokemon is dead.
@@ -65,7 +66,7 @@ class Pokemon:
             self._isAlive = False
             print(f"Pokemon {self._name} is dead!")
 
-    def earnXp(self, xp):
+    def earnXp(self, xp: int) -> None:
         """
         Training of a pokemon.
         Earns XP and increases the level of a pokemon.
@@ -76,40 +77,40 @@ class Pokemon:
             self._levelProgress -= 100
             self._levelUp()
 
-    def useHealthPotion(self):
+    def useHealthPotion(self) -> None:
         self._health = self._maxHealth
         print(f"Pokemon {self._name} was healed.")
 
-    def getName(self):
+    def getName(self) -> str:
         return self._name
 
-    def getPokeType(self):
+    def getPokeType(self) -> str:
         return self._pokeType
 
-    def getHealth(self):
+    def getHealth(self) -> int:
         return self._health
 
-    def getLevel(self):
+    def getLevel(self) -> int:
         return self._level
 
-    def getIsAlive(self):
+    def getIsAlive(self) -> bool:
         return self._isAlive
 
-    def getStrength(self):
+    def getStrength(self) -> int:
         return self._strength
 
     @staticmethod
-    def getAttackFactor(attackType, defendType):
+    def getAttackFactor(attackType: str, defendType: str) -> int:
         """
-        RReturn the attack factor for a combination of types.
+        Return the attack factor for a combination of types.
         Returns 1 if no special combination is available.
         """
         attackTypes = {
-            ("Water", "Fire"): 2,
-            ("Fire", "Water"): 0.5,
-            ("Plant", "Water"): 2,
-            ("Water", "Plant"): 0.5,
-            ("Fire", "Plant"): 2,
-            ("Plant", "Fire"): 0.5,
+            ("Water", "Fire"): 4,
+            ("Fire", "Water"): 1,
+            ("Plant", "Water"): 4,
+            ("Water", "Plant"): 1,
+            ("Fire", "Plant"): 4,
+            ("Plant", "Fire"): 1,
         }
-        return attackTypes.get((attackType, defendType), 1)
+        return attackTypes.get((attackType, defendType), 2)
